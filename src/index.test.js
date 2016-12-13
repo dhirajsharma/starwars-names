03f43b4113579e2d25d9b9d0955a76328608d90b
dhirajsharma <sharma.dhiraj414@gmail.com>
2016-12-13 13:27:36 -0500

    feat(random):

--- src/index.test.js
+++ src/index.test.js
@@ -19,10 +19,17 @@ describe('my-starwars-names', function(){
 	});
 
 	describe('random', function(){
-
 		it('should return a random item from the starWars.all', function(){
 			var randomItem = starWars.random();
 			expect(starWars.all).to.include(randomItem);
+		})
+
+		it('should return an array of random items if passed a number', function(){
+			var randomItem = starWars.random(3);
+			expect(randomItem).to.have.length(3);
+			randomItem.forEach(function(item){
+				expect(starWars.all).to.include(item);
+			})
 		})
 	})
 });
